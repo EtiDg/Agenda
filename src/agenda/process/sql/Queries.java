652a22169b22d797a904276484bc2ace9043b7f7
etienne <etienne.mace_de_gastines@insa-rouen.fr>
2018-12-11 18:57:19 +0100

    Modification des packages, creation de la base de données

--- src/agenda/process/sql/Queries.java
+++ src/agenda/process/sql/Queries.java
@@ -1,5 +1,90 @@
 package agenda.process.sql;
 
-public class Queries {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import agenda.process.object.*;
+
+public class Queries {
+	
+	// insertions
+	private static String ajoutReprise = 
+			"INSERT INTO Reprise(id, nom, heureDebut, duree, nomLieu, nomMonitrice, date) \n"
+			+ "VALUES (?,?,?,?,?,?,?);";
+	private static String ajoutGroupe =
+			"INSERT INTO Groupe(nom, isVacances, isTreve, idMR) \n"
+			+ "VALUES (?,?,?,?);";
+	private static String ajoutModeleDeReprise =
+			"INSERT INTO ModeleDeReprise(id, nom) \n"
+			+ "VALUES (?,?);";
+	private static String ajoutLieu =
+			"INSERT INTO Lieu(nom) \n"
+			+ "VALUES (?);";
+	private static String ajoutMonitrice =
+			"INSERT INTO Monitrice(nom) \n"
+			+ "VALUES (?);";
+	private static String ajoutCreneau =
+			"INSERT INTO Creneau(id, heureDebut, duree, date, nomMonitrice) \n"
+			+ "VALUES (?,?,?,?,?);";
+	private static String ajoutDate =
+			"INSERT INTO Date(date, isFerie) \n"
+			+ "VALUES (?,?);";
+	private static String ajoutVacances =
+			"INSERT INTO Vacances(nom, dateDebut, dateFin) \n"
+			+ "VALUES (?,?,?);";
+	private static String ajoutTreveHivernale =
+			"INSERT INTO TreveHivernale(dateDebut, dateFin) \n"
+			+ "VALUES (?,?);";
+	
+
+	public void ajoutReprises(Connection conn, PreparedStatement ps, ArrayList<Reprise> reprises){
+		ps = conn.prepareStatement(ajoutReprise);
+		for (int i = 0; i < reprises.size(); ++i){
+			ps.setInt(1,reprises.get(i).getId());
+			ps.setString(2,reprises.get(i).getNom());
+			ps.setInt(3,reprises.get(i).getHeureDebut());
+			ps.setInt(4,reprises.get(i).getDuree());
+			ps.setString(5,reprises.get(i).getNomLieu());
+			ps.setString(6,reprises.get(i).getNomMonitrice());
+			ps.setDate(7,reprises.get(i).getDate());
+			ps.addBatch();
+		}
+	}
+	
+	public void ajoutGroupe(Connection conn, PreparedStatement ps, Groupe groupe){
+		ps = conn.prepareStatement(ajoutGroupe);
+		ps.setString(1,groupe.getNom());
+		ps.setBoolean(2,groupe.getIsVacances());
+		ps.setBoolean(3,groupe.getIsTreveHivernale());
+	}
+	
+	public void ajoutModeleDeReprise(Connection conn, PreparedStatement ps, ModeleDeReprise modeleDeReprise){
+		ps = conn.prepareStatement(ajoutModeleDeReprise);
+		ps.setInt(1,modeleDeReprise.getId());
+		ps.setString(2,modeleDeReprise.getNom());
+	}
+	
+	public void ajoutLieu(Connection conn, PreparedStatement ps, Lieu lieu){
+		ps = conn.prepareStatement(ajoutLieu);
+		ps.setString(1,lieu.getNom());
+	}
+	
+	public void ajoutMonitrice(Connection conn, PreparedStatement ps, Monitrice monitrice){
+		ps = conn.prepareStatement(ajoutMonitrice);
+		ps.setString(1,monitrice.getNom());
+	}
+	
+	public void ajoutCreneau(Connection conn, PreparedStatement ps, Creneau creneau){
+		ps = conn.prepareStatement(ajoutMonitrice);
+		ps.setInt(1,creneau.getId());
+		ps.setString(1,creneau.getNom());
+		ps.setString(1,creneau.getNom());
+		ps.setString(1,creneau.getNom());
+
+	}
+	
+	
+	
 }
---
+++ src/agenda/process/sql/SQLiteJDBCDriverConnection.java
@@ -0,0 +1,127 @@
+package agenda.process.sql;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SQLiteJDBCDriverConnection {
+	private static String url = "jdbc:sqlite:database/agenda.db"; 
+	
+	public SQLiteJDBCDriverConnection(){
+		
+	}
+	
+    /**
+    * Connect to a sample database
+    */
+   public void connect() {
+       Connection conn = null;
+       try {
+           // create a connection to the database
+           conn = DriverManager.getConnection(url);
+           
+           System.out.println("Connection to SQLite has been established.");
+           
+       } catch (SQLException e) {
+           System.out.println(e.getMessage());
+       } finally {
+           try {
+               if (conn != null) {
+                   conn.close();
+               }
+           } catch (SQLException ex) {
+               System.out.println(ex.getMessage());
+           }
+       }
+   }
+   
+   public void createTables(){       
+       // SQL statement for creating new tables 
+       String creationModeleDeReprise = "CREATE TABLE IF NOT EXISTS ModeleDeReprise (\n"
+               + "	id integer PRIMARY KEY,\n"
+               + "	nom text\n"
+               + ");";
+       
+       String creationGroupe = "CREATE TABLE IF NOT EXISTS Groupe (\n"
+               + "	nom text PRIMARY KEY,\n"
+               + "	isVacances boolean,\n"
+               + "  isTreve boolean,\n"
+               + "  idMR integer,\n"
+               + "  FOREIGN KEY (idMR) REFERENCES ModeleDeReprise(id)\n"
+               + ");";
+       
+       String creationLieu = "CREATE TABLE IF NOT EXISTS Lieu (\n"
+               + "	nom text PRIMARY KEY\n"
+               + ");";
+       
+       String creationMonitrice = "CREATE TABLE IF NOT EXISTS Monitrice (\n"
+               + "	nom text PRIMARY KEY\n"
+               + ");";
+       
+       String creationDate = "CREATE TABLE IF NOT EXISTS Date (\n"
+               + "	date date PRIMARY KEY,\n"
+    		   + "  isFerie boolean\n"
+               + ");";
+       
+       String creationReprise = "CREATE TABLE IF NOT EXISTS Reprise (\n"
+               + "	id integer PRIMARY KEY,\n"
+               + "	nom text, \n"
+               + "  heureDebut integer,\n"
+               + "  duree integer,\n"
+               + "  nomLieu text ,\n"
+               + "  nomMonitrice text,\n "
+               + "  date date ,\n"
+               + "  FOREIGN KEY  (nomLieu) REFERENCES Lieu(nom),\n"
+               + "  FOREIGN KEY  (nomMonitrice) REFERENCES Monitrice(nom),\n"
+               + "  FOREIGN KEY  (date) REFERENCES Date(date)\n"
+               + ");";
+       
+       String creationCreneau = "CREATE TABLE IF NOT EXISTS Creneau (\n"
+               + "	id integer PRIMARY KEY,\n"
+               + "  heureDebut integer,\n"
+               + "  duree integer,\n"
+               + "  date date ,\n"
+               + "  nomMonitrice text,\n"
+               + "  FOREIGN KEY (date) REFERENCES Date(date),\n"
+               + "  FOREIGN KEY (nomMonitrice) REFERENCES Monitrice(nom)\n"
+               + ");";
+       
+       String creationVacances = "CREATE TABLE IF NOT EXISTS Vacances (\n"
+               + "	nom text PRIMARY KEY,\n"
+               + "  dateDebut date, \n"
+               + "  dateFin date, \n"
+               + "  FOREIGN KEY (dateDebut) REFERENCES Date(date),\n"
+               + "  FOREIGN KEY (dateFin) REFERENCES Date(date)\n"
+               + ");";
+       
+       String creationTreveHivernale = "CREATE TABLE IF NOT EXISTS TreveHivernale (\n"
+               + "  dateDebut date,\n" 
+               + "  dateFin date ,\n" 
+               + "  FOREIGN KEY (dateDebut) REFERENCES Date(date),\n"
+               + "  FOREIGN KEY (dateFin) REFERENCES Date(date)\n"
+               + ");";
+       
+       
+       try(Connection conn = DriverManager.getConnection(url);
+    	   Statement stmt = conn.createStatement();
+    		   ){
+		           // create the tables
+		    	   stmt.execute(creationModeleDeReprise);
+		    	   stmt.execute(creationGroupe);
+		    	   stmt.execute(creationLieu);
+		    	   stmt.execute(creationMonitrice);
+		    	   stmt.execute(creationDate);
+		    	   stmt.execute(creationReprise);
+		    	   stmt.execute(creationCreneau);
+		    	   stmt.execute(creationVacances);
+		    	   stmt.execute(creationTreveHivernale);
+
+	    	   }catch (SQLException e) {
+	           System.out.println(e.getMessage());
+       }
+   }
+}
+
